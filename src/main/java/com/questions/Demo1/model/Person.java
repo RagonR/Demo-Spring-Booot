1a62432c782449d6d2a8a56b59af397cb258b11b
RagonR <rapolasrusakevicius@gmail.com>
2021-04-17 18:22:29 +0300

    Hibernate implementation

--- src/main/java/com/questions/Demo1/model/Person.java
+++ src/main/java/com/questions/Demo1/model/Person.java
@@ -2,11 +2,20 @@ package com.questions.Demo1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
+@Entity(name = "person")
 public class Person {
+
+    @Id
+    @Column(name = "id")
     private final UUID id;
+
+    @Column(name = "name")
     @NotBlank
     private final String name;
 
