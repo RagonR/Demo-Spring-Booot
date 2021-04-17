37c5b1bccb56020b6c2294bde87e6c289107cb4f
dragonlais <rapolasrusakevicius@gmail.com>
2021-04-17 17:40:52 +0300

    Initial commit

---
+++ src/main/java/com/questions/Demo1/dao/PersonDataAccessService.java
@@ -0,0 +1,68 @@
+package com.questions.Demo1.dao;
+
+import com.questions.Demo1.model.Person;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        final String sql = "SELECT id, name FROM person";
+        List<Person> people = jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
+        return people;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{id},
+                (resultSet, i) -> {
+                    UUID personId = UUID.fromString(resultSet.getString("id"));
+                    String name = resultSet.getString("name");
+                    return new Person(personId, name);
+                });
+        return Optional.ofNullable(person);
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        final String sql = "DELETE FROM person WHERE id = ?";
+        Object[] args = new Object[] {id};
+        jdbcTemplate.update(sql, args);
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        final String sql = " UPDATE person person WHERE id = ?";
+        Object[] args = new Object[] {id};
+        jdbcTemplate.update(sql, args);
+        return 0;
+    }
+}
